c38069c8be0d978dee04f6ad67cb633dbd1460d5
Gustavo Colombini <gucolombini@gmail.com>
2025-08-12 17:35:51 -0300

    feat: question block scene instancing
    it currently spawns mario

--- scenes/objects/question_block.gd
+++ scenes/objects/question_block.gd
@@ -3,19 +3,27 @@ extends StaticBody2D
 var original_pos = position.y
 @export var bounce_velocity = -900
 @export var gravity = 5000
+@export var spawn_scene : PackedScene
+@export var total_uses = 1
 
 var bouncing = false
 
 var y_speed = 0.0
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if not bouncing:
+	if not bouncing && total_uses > 0:
 		bounce()
 	
 func bounce():
 	print("HIIII")
 	bouncing = true
 	y_speed = bounce_velocity
+	total_uses -= 1
+	var spawned_entity = spawn_scene.instantiate()
+	add_sibling(spawned_entity)
+	spawned_entity.position.x = position.x
+	spawned_entity.position.y = position.y-30
+	spawned_entity.velocity.y = -1500
 	
 
 func _ready():
