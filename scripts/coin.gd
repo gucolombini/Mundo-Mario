566d7f0837f5f304b77d7c1cd057c28abdce6a6d
Gustavo Colombini <gucolombini@gmail.com>
2025-12-03 14:41:53 -0300

    feat: coin particles
    will add outline to texture so it doesnt look as weird

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,10 +3,17 @@ extends Area2D
 signal coin_collected
 var collected: bool = false
 
+@export var particle_emitter : PackedScene
+
 func collect():
 	if not collected:
 		collected = true
 		emit_signal("coin_collected")
+		if particle_emitter: 
+			var particles = particle_emitter.instantiate()
+			particles.position.x = position.x
+			particles.position.y = position.y
+			get_parent().add_child(particles)
 		position.y = 9999
 		$SFXCoin.play()
 		await $SFXCoin.finished
